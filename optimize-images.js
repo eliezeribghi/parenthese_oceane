5670a12bc11f1a1eac2f27d435682cb1092e4cf6
eliezeribghi <120668513+eliezeribghi@users.noreply.github.com>
2024-06-05 15:53:48 +0200

    modification des image avec le fichier optimize

---
+++ optimize-images.js
@@ -0,0 +1,17 @@
+import imagemin from 'imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminWebp from 'imagemin-webp';
+
+(async () => {
+    const files = await imagemin(['public/assets/**/*.{jpg,jpeg,png}'], {
+        destination: 'output',
+        plugins: [
+            imageminMozjpeg({ quality: 75 }),
+            imageminPngquant({ quality: [0.6, 0.8] }),
+            imageminWebp({ quality: 75 })
+        ]
+    });
+
+    console.log('Images optimized:', files);
+})();
