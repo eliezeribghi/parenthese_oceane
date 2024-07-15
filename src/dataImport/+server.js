ff0273fb85366c49eac1bee6470d13a10c730fad
eliezeribghi <iibgui@yahoo.fr>
2024-07-15 15:27:44 +0200

    rajout de titre et alt dans le fichier +server.js

--- src/dataImport/+server.js
+++ src/dataImport/+server.js
@@ -3,55 +3,53 @@
 export {images};
 
 const images = [
-    {
-        alt: 'Description de l\'image 1',
-        src: './assets/imgCarousel/pictureHome.webp',
-        title: 'Titre de l\'image 1'
-      },
-      {
-        alt: 'Description de l\'image 2',
-        src: './assets/imgCarousel/pictureHome1.webp',
-        title: 'Titre de l\'image 2'
-      },
-
-      {
-        alt: 'Description de l\'image 3 ',
-        src: './assets/imgCarousel/pictureHome2.webp',
-        title: 'Titre de l\'image '
-      },
-
-      {
-        alt: 'Description de l\'image 3 ',
-        src: './assets/imgCarousel/pictureHome3.webp',
-        title: 'Titre de l\'image '
-      },
-      {
-        alt: 'Description de l\'image 3 ',
-        src: './assets/imgCarousel/pictureHome4.webp',
-        title: 'Titre de l\'image '
-      },
-      {
-        alt: 'Description de l\'image 3 ',
-        src: './assets/imgCarousel/pictureHome5.webp',
-        title: 'Titre de l\'image '
-      },
-      {
-        alt: 'Description de l\'image 3 ',
-        src: './assets/imgCarousel/pictureHome6.webp',
-        title: 'Titre de l\'image '
-      },
-      {
-        alt: 'Description de l\'image 3 ',
-        src: './assets/imgCarousel/pictureHome7.webp',
-        title: 'Titre de l\'image '
-      },
-    {
-      alt: 'Description de l\'image 3 ',
-      src: './assets/imgCarousel/pictureHome8.webp',
-      title: 'Titre de l\'image '
-    }
- 
+  {
+    alt: 'Gîte Marée Haute',
+    src: './assets/imgCarousel/pictureHome.webp',
+    title: 'Gîte Marée Haute - Parenthèse Oceaner.com',
+  },
+  {
+    alt: 'Plage Clémenceau',
+    src: './assets/imgCarousel/pictureHome1.webp',
+    title: 'Plage Clémenceau - Parenthèse Oceaner.com',
+  },
+  {
+    alt: 'Plage rocheuse avec vagues déferlantes et ciel nuageux',
+    src: './assets/imgCarousel/pictureHome2.webp',
+    title: 'Plage rocheuse avec vagues - Parenthèse Oceaner.com',
+  },
+  {
+    alt: 'Champ verdoyant en premier plan, avec vue sur un ciel nuageux et une mer agitée en arrière-plan',
+    src: './assets/imgCarousel/pictureHome3.webp',
+    title: 'Champ verdoyant avec vue sur mer - Parenthèse Oceaner.com',
+  },
+  {
+    alt: 'Aire de jeux pour enfants avec trampoline et toboggan près du gîte',
+    src: './assets/imgCarousel/pictureHome9.webp',
+    title: 'Aire de jeux enfants près du gîte - Parenthèse Oceaner.com',
+  },
+  {
+    alt: 'Commode blanche de chevet avec lampe allumée',
+    src: './assets/imgCarousel/pictureHome5.webp',
+    title: 'Commode blanche de chevet - Parenthèse Oceaner.com',
+  },
+  {
+    alt: 'Joli arbre solitaire au milieu d\'un champ verdoyant',
+    src: './assets/imgCarousel/pictureHome6.webp',
+    title: 'Joli arbre solitaire - Parenthèse Oceaner.com',
+  },
+  {
+    alt: 'Joli puits en pierre avec de l\'eau claire',
+    src: './assets/imgCarousel/pictureHome7.webp',
+    title: 'Joli puits en pierre - Parenthèse Oceaner.com',
+  },
+  {
+    alt: 'Femme et enfant regardant la mer',
+    src: './assets/imgCarousel/pictureHome8.webp',
+    title: 'Femme et enfant regardant la mer - Parenthèse Oceaner.com',
+  }
 ];
+
 
 
 
