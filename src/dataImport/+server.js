cc59b0f2909bf8e19e3c934dcf984d0eee546688
eliezeribghi <iibgui@yahoo.fr>
2024-07-29 13:19:59 +0200

    rajout dune photo pour le carousel home page

--- src/dataImport/+server.js
+++ src/dataImport/+server.js
@@ -1,63 +1,57 @@
-
-
-export {images};
+export { images };
 
 const images = [
   {
-    alt: 'Gîte Marée Haute',
-    src: './assets/imgCarousel/pictureHome.webp',
-    title: 'Gîte Marée Haute - Parenthèse Oceaner.com',
+    alt: "Gîte Marée Haute",
+    src: "./assets/imgCarousel/pictureHome.webp",
+    title: "Gîte Marée Haute - Parenthèse Oceaner.com",
   },
   {
-    alt: 'Plage Clémenceau',
-    src: './assets/imgCarousel/pictureHome1.webp',
-    title: 'Plage Clémenceau - Parenthèse Oceaner.com',
+    alt: "Plage Clémenceau",
+    src: "./assets/imgCarousel/pictureHome1.webp",
+    title: "Plage Clémenceau - Parenthèse Oceaner.com",
   },
   {
-    alt: 'Plage rocheuse avec vagues déferlantes et ciel nuageux',
-    src: './assets/imgCarousel/pictureHome2.webp',
-    title: 'Plage rocheuse avec vagues - Parenthèse Oceaner.com',
+    alt: "Plage rocheuse avec vagues déferlantes et ciel nuageux",
+    src: "./assets/imgCarousel/pictureHome2.webp",
+    title: "Plage rocheuse avec vagues - Parenthèse Oceaner.com",
   },
   {
-    alt: 'Champ verdoyant en premier plan, avec vue sur un ciel nuageux et une mer agitée en arrière-plan',
-    src: './assets/imgCarousel/pictureHome3.webp',
-    title: 'Champ verdoyant avec vue sur mer - Parenthèse Oceaner.com',
+    alt: "Champ verdoyant en premier plan, avec vue sur un ciel nuageux et une mer agitée en arrière-plan",
+    src: "./assets/imgCarousel/pictureHome3.webp",
+    title: "Champ verdoyant avec vue sur mer - Parenthèse Oceaner.com",
   },
   {
-    alt: 'Aire de jeux pour enfants avec trampoline et toboggan près du gîte',
-    src: './assets/imgCarousel/pictureHome9.webp',
-    title: 'Aire de jeux enfants près du gîte - Parenthèse Oceaner.com',
+    alt: "Aire de jeux pour enfants avec trampoline et toboggan près du gîte",
+    src: "./assets/imgCarousel/pictureHome9.webp",
+    title: "Aire de jeux enfants près du gîte - Parenthèse Oceaner.com",
   },
   {
-    alt: 'Commode blanche de chevet avec lampe allumée',
-    src: './assets/imgCarousel/pictureHome5.webp',
-    title: 'Commode blanche de chevet - Parenthèse Oceaner.com',
+    alt: "Un vélo avec un escabeau près d'un arbre",
+    src: "./assets/imgCarousel/pictureHome9.webp",
+    title: "Un vélo avec un escabeau près d'un arbre - Parenthèse Oceaner.com",
   },
-  {
-    alt: 'Joli arbre solitaire au milieu d\'un champ verdoyant',
-    src: './assets/imgCarousel/pictureHome6.webp',
-    title: 'Joli arbre solitaire - Parenthèse Oceaner.com',
-  },
-  {
-    alt: 'Joli puits en pierre avec de l\'eau claire',
-    src: './assets/imgCarousel/pictureHome7.webp',
-    title: 'Joli puits en pierre - Parenthèse Oceaner.com',
-  },
-  {
-    alt: 'Femme et enfant regardant la mer',
-    src: './assets/imgCarousel/pictureHome8.webp',
-    title: 'Femme et enfant regardant la mer - Parenthèse Oceaner.com',
-  },
-  {
-    "alt": "Un vélo avec un escabeau près d'un arbre",
-    "src": "./assets/imgCarousel/pictureHome9.webp",
-    "title": "Un vélo avec un escabeau près d'un arbre - Parenthèse Oceaner.com"
-}
 
+  {
+    alt: "Commode blanche de chevet avec lampe allumée",
+    src: "./assets/imgCarousel/pictureHome5.webp",
+    title: "Commode blanche de chevet - Parenthèse Oceaner.com",
+  },
+  {
+    alt: "Joli arbre solitaire au milieu d'un champ verdoyant",
+    src: "./assets/imgCarousel/pictureHome6.webp",
+    title: "Joli arbre solitaire - Parenthèse Oceaner.com",
+  },
+  {
+    alt: "Joli puits en pierre avec de l'eau claire",
+    src: "./assets/imgCarousel/pictureHome7.webp",
+    title: "Joli puits en pierre - Parenthèse Oceaner.com",
+  },
+  {
+    alt: "Femme et enfant regardant la mer",
+    src: "./assets/imgCarousel/pictureHome8.webp",
+    title: "Femme et enfant regardant la mer - Parenthèse Oceaner.com",
+  },
 ];
-
-
-
-
 
 export default images;
